45eba072ab4d33e6dfa46533a92b1d83a8f7d201
Sagar Lodhe <sagar1990ku@gmail.com>
2024-08-29 14:43:53 +0530

    fixerror

--- backend/Utils/generateToken.js
+++ backend/Utils/generateToken.js
@@ -8,6 +8,6 @@ export const generateTokenSetCoookie =  (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
-    secure: process.env.NODE_ENV !== "devlopment",
+    secure: process.env.NODE_ENV !== "development",
   });
 };
